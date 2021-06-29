0733c873a30e38232e3f143f5b2d9cebc1f6a5db
Your Name <club11@bk.ru>
2021-06-30 02:28:52 +0300

    added register half-made v0/1  30/06/2021

--- src/users/admin.py
+++ src/users/admin.py
@@ -3,6 +3,21 @@ from . import models
 
 # Register your models here.
 
+class Profile(admin.ModelAdmin):
+    list_display = [
+        'pk',
+        'user',
+        'tel',
+        'first_name',
+        'last_name',
+        'email',
+        'user_group',
+        'home_adress',
+        # need extention with  country city index adress1 adress2
+        'another_info',  
+    ]  
+
+
 admin.site.register(models.Profile)
 
 
