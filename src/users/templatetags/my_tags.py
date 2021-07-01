2c2f6309a680da4f09cea131d46bd8f1ed4eb4b6
Your Name <club11@bk.ru>
2021-07-02 01:04:49 +0300

    added register half-made with currency  01/07/2021

---
+++ src/users/templatetags/my_tags.py
@@ -0,0 +1,10 @@
+from django import template
+import requests
+register = template.Library()
+
+USD_ENDPOINT = 'https://www.nbrb.by/api/exrates/rates/145'
+
+@register.simple_tag
+def currency_rate():
+    res = requests.get(USD_ENDPOINT)
+    return res.json().get('Cur_OfficialRate')
